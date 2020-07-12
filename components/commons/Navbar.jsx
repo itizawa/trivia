5d1eca27a02348d9359d66c296a22fa8ec87749b
itizawa <itizawa@weseek.co.jp>
2020-07-12 11:34:15 +0900

    add create button

--- components/commons/Navbar.jsx
+++ components/commons/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
+
+import Pencil from './atoms/svg/Pencil';
 
 function Navbar() {
   return (
     <nav className="navbar bg-orange">
       <div className="container-fluid">
-        <a className="navbar-brand text-white" href="#">
-          Summary Post
-        </a>
+        <Link href="/">
+          <a className="navbar-brand text-white">
+            Summary Post
+          </a>
+        </Link>
+        <Link href="/new">
+          <a className="text-white">
+            <Pencil width="24px" height="24px" />
+            <span className="ml-1">作成する</span>
+          </a>
+        </Link>
       </div>
     </nav>
   );
