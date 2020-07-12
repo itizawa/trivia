c83e7731f588dcb3caa6027ac84b6591668eb300
itizawa <itizawa@weseek.co.jp>
2020-07-12 14:34:39 +0900

    add form

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,10 +10,10 @@ const IndexPage = () => (
       簡単に要約を作れるサイトです。Twitter連携で作ったサマリーをTweetできます。<br />あなたの言葉が世界の理を紡ぐ....
       <div className="my-5">
         <div className="d-flex justify-content-around">
-          <button type="button" className="btn btn-orange" onClick={() => { Router.push('/new') }}>
+          <button type="button" className="btn btn-orange text-white" onClick={() => { Router.push('/new') }}>
             サマリーを作る
           </button>
-          <button type="button" className="btn btn-teal" onClick={() => { Router.push('/list') }}>
+          <button type="button" className="btn btn-teal text-white" onClick={() => { Router.push('/list') }}>
             サマリーを見る
           </button>
         </div>
---
+++ pages/new.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+function Page() {
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+
+  function onClickSubmit() {
+    // TODO #8 post request
+  }
+
+  return (
+    <div className="bg-snow rounded mt-3 p-3">
+      <h1 className="text-center">新規サマリーを作成する</h1>
+      <form className="mt-3">
+        <div className="mb-3">
+          <label htmlFor="summaryTitle" className="form-label">タイトル</label>
+          <input
+            type="text"
+            className="form-control"
+            id="summaryTitle"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+            aria-describedby="title"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="summaryText" className="form-label">本文</label>
+          <textarea
+            type="text"
+            className="form-control"
+            id="summaryText"
+            value={text}
+            onChange={e => setText(e.target.value)}
+            rows="10"
+          />
+        </div>
+        <div className="text-right">
+          <button type="button" className="btn btn-orange text-snow" onClick={onClickSubmit}>
+            作成する！
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Page;
