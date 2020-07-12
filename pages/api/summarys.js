39e94ca885f58aca0f6d765204433c9979540092
itizawa <itizawa@weseek.co.jp>
2020-07-12 20:06:48 +0900

    add validator

--- pages/api/summarys.js
+++ pages/api/summarys.js
@@ -1,30 +1,35 @@
+
+import nextConnect from 'next-connect';
+import validator from 'validator';
+import ApiValidator from '@middlewares/ApiValidator';
+import { body } from 'express-validator';
+
 import Summary from '@models/Summary';
-import dbConnect from '@lib/dbConnect';
+import dbConnect from '@middlewares/dbConnect';
+
+const handler = nextConnect();
 
 dbConnect();
 
-export default async(req, res) => {
-  const post = async() => {
-    const { title, text } = req.body;
-    try {
-      const newPost = new Summary({
-        title,
-        text,
-      });
-      await newPost.save();
-      return res.status(200).send({});
-    }
-    catch (err) {
-      console.log(err);
-      return res.status(400).send({ success: false });
-    }
-  };
+validator.summary = [
+  body('title').isString().isLength({ min: 1, max: 40 }).withMessage('hoge'),
+  body('text').isString().isLength({ min: 5, max: 1000 }).withMessage('hoge'),
+];
 
-  switch (req.method) {
-    case 'POST':
-      return post();
-    default:
-      res.status(405).end(`Method ${req.method} Not Allowed`);
-      break;
+handler.post(validator.summary, ApiValidator, async(req, res) => {
+  const { title, text } = req.body;
+  try {
+    const newPost = new Summary({
+      title,
+      text,
+    });
+    await newPost.save();
+    return res.status(200).send({});
   }
-};
+  catch (err) {
+    console.log(err);
+    return res.status(400).send({ success: false });
+  }
+});
+
+export default handler;
