2b258be151bfc2576c1f8680b1c0e1f0f33363d7
itizawa <itizawa@weseek.co.jp>
2020-07-19 19:59:36 +0900

    add onClick function

--- components/commons/TriviaModal.jsx
+++ components/commons/TriviaModal.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, ModalBody } from 'reactstrap';
 
 import TriviaListContainer from '@containers/TriviaListContainer';
 
 function TriviaModal() {
-  const { triviaForModal, closeTriviaModal } = TriviaListContainer.useContainer();
+  const { triviaForModal, closeTriviaModal, isOpenTriviaModal } = TriviaListContainer.useContainer();
+  const [count, setCount] = useState(0);
+  const totalCount = count + triviaForModal?.acquisitionCount;
+
+  function closeModalHandler() {
+    setCount(0);
+    if (closeTriviaModal != null) {
+      closeTriviaModal();
+    }
+  }
 
   return (
-    <Modal size="lg" isOpen={triviaForModal != null} toggle={closeTriviaModal} className="trivia-modal">
+    <Modal size="lg" isOpen={isOpenTriviaModal} toggle={closeModalHandler} className="trivia-modal">
       <ModalBody className="trivia-modal-body text-center p-5 d-flex align-items-center">
         <div className="w-100">
           {triviaForModal?.forwardText}
@@ -17,16 +26,21 @@ function TriviaModal() {
       </ModalBody>
       <div className="d-flex p-3">
         <div className="col-4 align-bottom d-flex align-items-center">
-          0 へえ
+          {count} へえ
         </div>
         <div className="col-4 text-center">
-          <button type="button" className="btn btn-info btn-trivia text-snow rounded-circle">
+          <button
+            type="button"
+            className="btn btn-info btn-trivia text-snow rounded-circle"
+            onClick={() => { setCount(count + 1) }}
+            disabled={count >= 20}
+          >
             へぇ
           </button>
         </div>
         <div className="col-4 text-right">
           by {triviaForModal?.userName}<br />
-          合計 {triviaForModal?.acquisitionCount} へえ
+          合計 {totalCount} へえ
         </div>
       </div>
     </Modal>
