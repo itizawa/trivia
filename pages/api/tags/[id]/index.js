ca05abfafa83ee1ac6dadfb4f6f78fef9b5fbacd
itizawa <itizawa@weseek.co.jp>
2020-09-06 01:24:30 +0900

    add getServerSideProps

---
+++ pages/api/tags/[id]/index.js
@@ -0,0 +1,32 @@
+
+import nextConnect from 'next-connect';
+import validator from 'validator';
+import ApiValidator from '@middlewares/ApiValidator';
+import { query } from 'express-validator';
+
+import Tag from '@models/Tag';
+import dbConnect from '@middlewares/dbConnect';
+
+const handler = nextConnect();
+
+dbConnect();
+
+validator.paginate = [
+  query('id').isMongoId(),
+];
+
+handler.get(validator.paginate, ApiValidator, async(req, res) => {
+  const { id } = req.query;
+
+  try {
+    const tag = await Tag.find({ _id: id });
+
+    return res.status(200).send({ tag });
+  }
+  catch (err) {
+    return res.status(500).send({ success: false });
+  }
+});
+
+
+export default handler;
--- pages/tags/[id].jsx
+++ pages/tags/[id].jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import axios from 'axios';
 
 import TriviaList from '@components/Trivia/TriviaList';
 
@@ -19,6 +20,27 @@ function ListPage(props) {
       </div>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+
+  let tag;
+
+  const hostUrl = process.env.SITE_URL || 'http://localhost:3000';
+
+  try {
+    const res = await axios.get(`${hostUrl}/api/tags/${params.id}`);
+    tag = res.data.tag;
+  }
+  catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+
+  return {
+    props: { tag }, // will be passed to the page component as props
+  };
 }
 
 ListPage.propTypes = {
