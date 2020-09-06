6bd8b0625412290e10ccf9074275074456d85f9d
itizawa <itizawa@weseek.co.jp>
2020-09-06 12:01:16 +0900

    adjust dropdown

--- components/Navbar/PersonalDropdown.jsx
+++ components/Navbar/PersonalDropdown.jsx
@@ -11,9 +11,9 @@ function PersonalDropdown() {
       <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
         <img height="24px" className="rounded-circle bg-white mr-2" src={user.image} />
       </button>
-      <ul className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-        <li><a className="dropdown-item" onClick={() => { signout() }}>Logout</a></li>
-      </ul>
+      <div className="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+        <button className="dropdown-item" type="button" onClick={() => { signout() }}>Logout</button>
+      </div>
     </div>
   );
 }
