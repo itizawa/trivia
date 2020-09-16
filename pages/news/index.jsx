31d09497cc6068c356e0adf278f33a8127ab07a4
itizawa <itizawa@weseek.co.jp>
2020-09-17 00:42:33 +0900

    create news list page

--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 
-const IndexPage = () => (
-  <>
-    <Head>
-      <title>News</title>
-    </Head>
-    <div className="bg-snow rounded mt-3 p-3">
-      <h1 className="text-center border-bottom mb-3">News 一覧</h1>
-      <ul className="list-group">
-        <li className="list-group-item">β版をリリースしました</li>
-      </ul>
-    </div>
-  </>
-);
+import newsList from '../../lib/newsList';
 
-export default IndexPage;
+const NewsPage = () => {
+  return (
+    <>
+      <Head>
+        <title>News</title>
+      </Head>
+      <div className="bg-snow rounded mt-3 p-3">
+        <h1 className="text-center border-bottom mb-3">News 一覧</h1>
+        <ul className="list-group">
+          {newsList.map((news) => {
+          return (
+            <li className="list-group-item" key={news.id}>
+              {news.createdAt}<br />
+              <Link href={`/news/${news.id}`}>
+                <a>
+                  {news.title}
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default NewsPage;
