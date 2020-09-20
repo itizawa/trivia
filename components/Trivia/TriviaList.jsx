1385d914c49c8f5b0d4634c2a86e391ed4d937f1
itizawa <itizawa@weseek.co.jp>
2020-09-20 09:52:03 +0900

    refactor

--- components/Trivia/TriviaList.jsx
+++ components/Trivia/TriviaList.jsx
@@ -6,51 +6,6 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import appContainer from '@containers/appContainer';
 import TriviaCard from '@components/Trivia/TriviaCard';
 import TriviaModal from '@components/Trivia/TriviaModal';
-import { useTriviaList } from '../Fooks/swrApi';
-
-function TriviaListBlock(props) {
-  const [triviaForModal, setTriviaForModal] = useState(null);
-
-  let url;
-  if (props.tagId != null) {
-    url = `/tags/${props.tagId}/pages`;
-  }
-  else {
-    url = '/trivias/list';
-  }
-  const { data, error } = useTriviaList(`${url}?page=${props.index}`, props.index);
-
-  if (error) return <div>failed to load</div>;
-
-  if (!data) {
-    const Skeletons = [];
-
-    for (let index = 0; index < 10; index++) {
-      Skeletons.push(<Skeleton key={index} className="box-fix-aspect mb-3" />);
-    }
-
-    return (
-      <>
-        {Skeletons}
-      </>
-    );
-  }
-
-  const triviasList = data?.docs;
-
-  return (
-    <>
-      {triviasList.map((trivia) => {
-        return (
-          <div className="mb-3" key={trivia._id}>
-            <TriviaCard trivia={trivia} onClickTriviaCard={onClickTriviaCard} />
-            <TriviaModal trivia={trivia} isOpen={triviaForModal === trivia._id} onClose={onCloseModal} />
-          </div>
-        );
-      })}
-    </>
-  );
-}
 
 function TriviaList(props) {
   const [triviaForModal, setTriviaForModal] = useState(null);
@@ -75,7 +30,6 @@ function TriviaList(props) {
         return setTriviasList(data.docs);
       }
       setTriviasList([...triviasList, ...data.docs]);
-      console.log(data);
     }
     fetchTriviaList();
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -95,11 +49,6 @@ function TriviaList(props) {
     );
   }
 
-  async function fetchMoreData() {
-    setActivePage(activePage + 1);
-  }
-
-
   /**
    * open trivia modal
    * @param {string} id id of trivia
@@ -118,8 +67,8 @@ function TriviaList(props) {
 
   return (
     <InfiniteScroll
-      dataLength={triviasList.length} // This is important field to render the next data
-      next={fetchMoreData}
+      dataLength={triviasList.length}
+      next={() => setActivePage(activePage + 1)}
       hasMore={hasmore}
       loader={<Skeleton className="box-fix-aspect mb-3" />}
       endMessage={<p className="text-center">これで全てです</p>}
