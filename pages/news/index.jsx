f5068137c6d8657a315b7bee10bd319e2761aa8b
itizawa <itizawa@weseek.co.jp>
2020-09-23 23:21:57 +0900

    create release page

--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -12,6 +12,16 @@ function NewsPage() {
         <h1 className="text-center border-bottom mb-3">News 一覧</h1>
         <ul className="list-group">
           <li className="list-group-item">
+            2020/09/24<br />
+            <Link href="/news/20200924">
+              <a>
+                v1.0.1 にアップデートしました
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <ul className="list-group">
+          <li className="list-group-item">
             2020/09/22<br />
             <Link href="/news/20200922">
               <a>
