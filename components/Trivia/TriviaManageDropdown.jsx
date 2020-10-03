1f6a7dfa5164a85c4f33a1012f111f2732bf3401
itizawa <itizawa@weseek.co.jp>
2020-10-03 19:14:53 +0900

    create swal

--- components/Trivia/TriviaManageDropdown.jsx
+++ components/Trivia/TriviaManageDropdown.jsx
@@ -1,68 +1,52 @@
 
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Router from 'next/router';
 
-import {
-  Modal, ModalHeader, ModalBody, ModalFooter,
-} from 'reactstrap';
+import Swal from 'sweetalert2';
 
 import appContainer from '@containers/appContainer';
 
-import { toastError } from '@utils/toaster';
-
 import GearIcon from '../commons/icons/GearIcon';
-import TrashIcon from '../commons/icons/TrashIcon';
 
 function TriviaManageDropdown(props) {
   const { apiDelete } = appContainer.useContainer();
 
-  const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
-
-  function toggleDeleteModalHandler() {
-    setIsOpenDeleteModal(!isOpenDeleteModal);
-  }
-
-  async function deleteTriviaHandler() {
-    try {
-      await apiDelete(`/trivias/${props.triviaId}`);
-      Router.push('/list');
-    }
-    catch (error) {
-      toastError(error, 'Error');
-    }
-    setIsOpenDeleteModal(!isOpenDeleteModal);
+  async function showAlertHandler() {
+    Swal.fire({
+      title: 'Trivia を削除しますか?',
+      text: '一度消した Trivia は元には戻せません。',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#d33',
+      confirmButtonText: '削除する!',
+    }).then(async(result) => {
+      if (result.isConfirmed) {
+        await apiDelete(`/trivias/${props.triviaId}`);
+        Swal.fire(
+          '削除しました!',
+          'リストページに戻ります。',
+          'success',
+        ).then(() => {
+          Router.push('/list');
+        });
+      }
+    });
   }
 
   return (
-    <>
-      <div className="btn-group">
-        <button type="button" className="btn btn-outline-light btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <GearIcon />
-        </button>
-        <div className="dropdown-menu dropdown-menu-right">
-          <button className="dropdown-item" type="button" onClick={toggleDeleteModalHandler}>
-            <span className="ml-2">
-              削除する
-            </span>
-          </button>
-        </div>
-      </div>
-      <Modal size="lg" isOpen={isOpenDeleteModal} toggle={toggleDeleteModalHandler}>
-        <ModalHeader className="bg-danger text-snow">
-          <TrashIcon />
+    <div className="btn-group">
+      <button type="button" className="btn btn-outline-light btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <GearIcon />
+      </button>
+      <div className="dropdown-menu dropdown-menu-right">
+        <button className="dropdown-item" type="button" onClick={showAlertHandler}>
           <span className="ml-2">
-            削除します
+            削除する
           </span>
-        </ModalHeader>
-        <ModalBody>
-          削除します(一度消した物は元には戻せません)
-        </ModalBody>
-        <ModalFooter>
-          <button type="button" className="btn btn-danger" onClick={deleteTriviaHandler}>削除します</button>
-        </ModalFooter>
-      </Modal>
-    </>
+        </button>
+      </div>
+    </div>
   );
 }
 
