2583eec4a117d276ab4395a4c7fb66bc1967ebc3
itizawa <itizawa@weseek.co.jp>
2020-10-04 22:47:19 +0900

    create generateLieDownText

--- __test__/utils/generateText.test.js
+++ __test__/utils/generateText.test.js
@@ -19,3 +19,23 @@ describe('generateCleanText', () => {
     });
   });
 });
+
+describe('generateLieDownText', () => {
+
+  describe.each`
+  input        | expected
+  ${''}  | ${''}
+  ${null}  | ${''}
+  ${'hoge'}  | ${'hoge'}
+  ${'hoge<hugapiyo'}  | ${'hoge<hugapiyo'}
+  ${'hogehuga>piyo'}  | ${'hogehuga>piyo'}
+  ${'hoge<huga>piyo'}  | ${'hoge○○○○piyo'}
+  ${'hoge<huga><piyo>'}  | ${'hoge○○○○○○○○'}
+
+`('generateLieDownText', ({ input, expected }) => {
+    test(`returns ${expected}`, () => {
+      const result = generateLieDownText(input);
+      expect(result).toBe(expected);
+    });
+  });
+});
