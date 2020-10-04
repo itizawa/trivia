eef4e881693045d98a1f9a0f216f008e24735e8d
itizawa <itizawa@weseek.co.jp>
2020-10-04 22:43:45 +0900

    create generateCleanText

--- __test__/utils/generateText.test.js
+++ __test__/utils/generateText.test.js
@@ -1,6 +1,21 @@
+const { generateCleanText, generateLieDownText } = require('../../lib/utils/generateText');
 
-describe('Home', () => {
-  test('h1タグが描画されること', () => {
-    expect('a').toEqual('Welcome to Next!');
+describe('generateCleanText', () => {
+
+  describe.each`
+  input        | expected
+  ${''}  | ${''}
+  ${null}  | ${''}
+  ${'hoge'}  | ${'hoge'}
+  ${'hoge<hugapiyo'}  | ${'hogehugapiyo'}
+  ${'hogehuga>piyo'}  | ${'hogehugapiyo'}
+  ${'hoge<huga>piyo'}  | ${'hogehugapiyo'}
+  ${'hoge<huga><piyo>'}  | ${'hogehugapiyo'}
+
+`('generateCleanText', ({ input, expected }) => {
+    test(`returns ${expected}`, () => {
+      const result = generateCleanText(input);
+      expect(result).toBe(expected);
+    });
   });
 });
